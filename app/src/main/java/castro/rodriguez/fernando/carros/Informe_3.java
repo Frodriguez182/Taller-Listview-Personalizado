11fecb062a59669de06e5cf37c05981a84d5b351
Fernando Rodriguez <fejota@hotmail.com>
2017-05-07 15:44:31 -0500

    Activity agregadas

---
+++ app/src/main/java/castro/rodriguez/fernando/carros/Informe_3.java
@@ -0,0 +1,13 @@
+package castro.rodriguez.fernando.carros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Informe_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informe_3);
+    }
+}
